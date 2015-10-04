8fa6906fd3b9d74f9655e47a4969129783cab59d
Ian Nastajus <ian.nastajus@gmail.com>
2015-10-03 20:48:05 -0400

    :moon: moves, but only in discrete steps :unamused:

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ *  This is attached directly to GameManager, issuing BroadcastMessages, thus requiring any 
+ *  receiving GameObject scripts to be a child of this GameManager. For example, 
+ *  BroadcastMessage("Move", -1) will be received on the Moon class Move(float), because Moon
+ *  is a child (eventually) of GameManager.
+ */
+public class ButtonManager : MonoBehaviour {
+
+    public void LeftPressed()
+    {
+        BroadcastMessage("Move", -1);
+    }
+
+    public void RightPressed()
+    {
+        BroadcastMessage("Move", 1);
+    }
+}
---
+++ Assets/Scripts/UIAdapter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ *  This class exists as a layer of abstraction between the GameManager class and the
+ *  Canvas' UI's Button's Script's component OnClick() 
+ *  This way, only the ButtonManager methods are listed in the Unity Editor, which now requires an 
+ *  explicit decision whether or not to include an adapter to them.
+ */
+public class UIAdapter : MonoBehaviour {
+
+    ButtonManager bm; 
+
+    void Start()
+    {
+        GameObject gm = GameObject.Find("GameManager");
+        if (gm == null) { Debug.LogWarning("Game Manager not found! Cannot issue UI commands!"); return; }
+
+        bm = gm.GetComponent<ButtonManager>();
+        if (bm == null) { Debug.LogWarning("Button Manager script not found! Cannot issue UI commands!"); return; }
+    }
+
+    public void OnClickLeft()
+    {
+        if (bm == null) { Debug.LogWarning("Button Manager not found!"); return; }
+        bm.LeftPressed();
+    }
+    public void OnClickRight()
+    {
+        if (bm == null) { Debug.LogWarning("Button Manager not found!"); return; }
+        bm.RightPressed();
+    }
+
+}
